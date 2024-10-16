45753334a7ec8304149b658c7d38ed228e4c785a
Legrand Thomas <legrand.thomas5@hotmail.fr>
2024-10-16 14:55:02 +0200

    Add S3 upload provider plugin

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,12 +1,27 @@
+const UPLOAD_URL = `https://${process.env.AWS_S3_UPLOAD_BUCKET_NAME}.s3.${process.env.AWS_REGION}.scw.cloud/strapi-uploads`;
+
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", UPLOAD_URL],
+          "media-src": ["'self'", "data:", "blob:", UPLOAD_URL],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,1 +1,23 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        credentials: {
+          accessKeyId: env("AWS_ACCESS_KEY_ID"),
+          secretAccessKey: env("AWS_SECRET_ACCESS_KEY"),
+        },
+        region: env("AWS_REGION"), // e.g "fr-par"
+        endpoint: env("AWS_S3_UPLOAD_BUCKET_URL"), // e.g. "https://s3.fr-par.scw.cloud"
+        params: {
+          Bucket: env("AWS_S3_UPLOAD_BUCKET_NAME"),
+        },
+      },
+    },
+    actionOptions: {
+      upload: {},
+      uploadStream: {},
+      delete: {},
+    },
+  },
+});
